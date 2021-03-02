8284522bbc10127ca6229a499e1393281df8c850
Emir Muminoglu <me@emir.pw>
2021-03-02 20:21:39 +0300

    removed unused files

--- handler.go
+++
@@ -1,9 +0,0 @@
-package emir
-
-type (
-	// RequestHandler must process incoming requests
-	RequestHandler func(*Context) error
-
-	// ErrorHandler must process errror returned by RequestHandler.
-	ErrorHandler func(*Context, error)
-)
--- validator.go
+++
@@ -1,6 +0,0 @@
-package emir
-
-// Validator is the interface that wraps the Validate method.
-type Validator interface {
-	Validate(i interface{}) error
-}
